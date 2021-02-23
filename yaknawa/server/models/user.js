955e34682f63ccce382d06aaaf391b642a2a0bd7
KimKeunSoo <gs0351@naver.com>
2021-02-23 22:42:50 +0900

    add pharmacy, share

--- yaknawa/server/models/user.js
+++ yaknawa/server/models/user.js
@@ -2,34 +2,34 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  // 1.
+  // 1. ID
   id: {
     type: String,
     required: true,
   },
-  // 2.
+  // 2. password
   password: {
     type: String,
     required: true,
   },
-  // 3.
+  // 3. 생년월일
   birthDay: {
     type: Date,
   },
-  // 4.
+  // 4. 성별
   sex: {},
-  // 5.
+  // 5. 관심 의약품
   interestedM: {},
-  // 6.
+  // 6. 닉네임
   nickName: {
     type: String,
     required: true,
   },
-  // 7.
+  // 7. 이메일 주소
   email: {
     type: String,
     required: true,
   },
 });
 
-module.exports = mongoose.model("User", userSchema); // 4
+module.exports = mongoose.model("User", userSchema);
