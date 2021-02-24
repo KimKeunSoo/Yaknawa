54df09b96da834ddb74d6e3656c669dae5b7ced3
KimKeunSoo <gs0351@naver.com>
2021-02-25 00:55:56 +0900

    login get/post complete

--- yaknawa/server/server_ks.js
+++ yaknawa/server/server_ks.js
@@ -1,31 +1,85 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const app = express();
+const dbConfig = require("./config/db.config");
 
 var corsOptions = {
   origin: "http://localhost:8081",
 };
 
+const app = express();
+const PORT = process.env.PORT || 8080;
+
 // Express 앱을 만들기 위해 body-parser와 cors를 app.use를 사용해서 추가함
 app.use(cors(corsOptions));
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+const db = require("./models");
+const Role = db.role;
+
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+  function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+      if (!err && count === 0) {
+        new Role({
+          name: "user"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'user' to roles collection");
+        });
+  
+        new Role({
+          name: "moderator"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'moderator' to roles collection");
+        });
+  
+        new Role({
+          name: "admin"
+        }).save(err => {
+          if (err) {
+            console.log("error", err);
+          }
+  
+          console.log("added 'admin' to roles collection");
+        });
+      }
+    });
+  }
+
+//routes
+require("./routes/auth.routes")(app);
+require("./routes/user.routes")(app);
 
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "home page" });
 });
 
-//routes
-require("./routes/auth.routes")(app);
-require("./routes/user.routes")(app);
-
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
