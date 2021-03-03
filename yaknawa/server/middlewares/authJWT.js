dad30b47b55283acc80c3907c52e7fb5038a6641
mindol8 <37296580+mindol8@users.noreply.github.com>
2021-03-03 18:45:41 +0900

    20210303-수정2

---
+++ yaknawa/server/middlewares/authJWT.js
@@ -0,0 +1,93 @@
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth.config.js");
+const db = require("../models");
+const User = db.user;
+const Role = db.role;
+
+//token이 제공되는지 확인하여 legal한지 확인
+verifyToken = (req, res, next) => {
+  //HTTP 헤더의 x-access-token 확인
+  let token = req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
+
+  //verify()로 권한 확인
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
+
+isAdmin = (req, res, next) => {
+  User.findById(req.userId).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    Role.find(
+      {
+        _id: { $in: user.roles },
+      },
+      (err, roles) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "admin") {
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Require Admin Role!" });
+        return;
+      }
+    );
+  });
+};
+
+isModerator = (req, res, next) => {
+  User.findById(req.userId).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+
+    Role.find(
+      {
+        _id: { $in: user.roles },
+      },
+      (err, roles) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
+
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "moderator") {
+            next();
+            return;
+          }
+        }
+
+        res.status(403).send({ message: "Require Moderator Role!" });
+        return;
+      }
+    );
+  });
+};
+
+const authJwt = {
+  verifyToken,
+  isAdmin,
+  isModerator,
+};
+module.exports = authJwt;
---
+++ yaknawa/server/models/share.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const shareSchema = new Schema({
+  // 1. 약국 이름
+  pharmacyName: {
+    type: String,
+    required: true,
+  },
+  // 2. 약 이름
+  medicineName: {
+    type: String,
+    required: true,
+  },
+  // 3. 수량(정)
+  volume: {
+    type: Number,
+    required: true,
+  },
+  // 4. 구매 가격
+  purchasePrice: {
+    type: Number,
+    required: true,
+  },
+  // 5. 구매 일자
+  purchaseDate: {
+    type: Date,
+    required: true,
+  },
+  // 6. 작성자 ID
+  writer: {
+    type: String,
+    required: true,
+  },
+  // 7. 약국 리뷰
+  review: {
+    type: String,
+  },
+  // 8. 영수증 첨부
+  receiptPath: {},
+});
+
+module.exports = mongoose.model("Share", shareSchema);
